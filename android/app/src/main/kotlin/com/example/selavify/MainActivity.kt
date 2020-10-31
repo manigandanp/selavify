ff65c231d5cea8f8e3057d89ba9950a6f7c466fc
Manigandan P <manigandan.p@avalara.com>
2020-10-31 15:27:44 +0530

    add folder structure with dbservice

---
+++ android/app/src/main/kotlin/com/example/selavify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.selavify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Selavify",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
