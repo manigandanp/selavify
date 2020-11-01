292d9581d1c81273b1cae647b7acb05358c0b7df
Manigandan P <manigandan.p@avalara.com>
2020-11-01 19:20:11 +0530

    update routes and WidgetFlutterBindings in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:selavify/pages/add_transaction.dart';
+import 'package:selavify/pages/transaction_list.dart';
+import 'package:selavify/services/db_service.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-  ));
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DBService.init();
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Selavify",
-          style: TextStyle(fontSize: 24),
-        ),
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(),
+            enabledBorder: OutlineInputBorder(),
+            focusedBorder: OutlineInputBorder()),
       ),
-    );
-  }
+      debugShowCheckedModeBanner: false,
+      home: TransactionList(),
+      initialRoute: '/',
+      routes: {
+        TransactionList.routeName: (context) => TransactionList(),
+        AddTransaction.routeName: (context) => AddTransaction(),
+      },
+    ),
+  );
 }
