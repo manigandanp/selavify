c3e4a151a8cb7c2f162045789409f7c42dd5a1e4
Manigandan P <manigandan.p@avalara.com>
2020-11-01 19:13:21 +0530

    update transaction service with addTransaction method

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,10 +1,21 @@
 import 'package:selavify/models/transaction.dart';
 import 'package:selavify/services/db_service.dart';
+import 'package:uuid/uuid.dart';
 
 class TransactionService {
-  Future<List<Transaction>> fetchTransactions() async {
+  static Future<List<Transaction>> fetchTransactions() async {
     List<Map<String, dynamic>> _results =
         await DBService.query(Transaction.tableName);
     return _results.map((tr) => Transaction.fromJson(tr)).toList();
+  }
+
+  static Future<int> addTransaction(tr) async {
+    final metaData = {
+      "id": new Uuid().v4(),
+      "createdTimestamp": DateTime.now().millisecondsSinceEpoch,
+      "updatedTimestap": DateTime.now().millisecondsSinceEpoch,
+    };
+    return await DBService.insert(
+        Transaction.tableName, Transaction.fromJson({...tr, ...metaData}));
   }
 }
