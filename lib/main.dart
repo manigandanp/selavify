af26ad6e8af229d214a8ce6ef4fe72a1a0671a7f
Manigandan P <manigandan.p@avalara.com>
2020-11-22 21:32:04 +0530

    update main routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:selavify/pages/add_transaction.dart';
-import 'package:selavify/pages/transaction_list.dart';
-import 'package:selavify/services/db_service.dart';
+import 'package:provider/provider.dart';
+import 'package:selavify/models/app_database.dart';
+import 'package:selavify/models/dao.dart';
+import 'package:selavify/pages/categories.dart';
+import 'package:selavify/pages/sources.dart';
+import 'package:selavify/pages/transaction_type.dart';
+import 'package:selavify/pages/transactions.dart';
+// import 'package:selavify/pages/add_transaction.dart';
+// import 'package:selavify/pages/transaction_list.dart';
+// import 'package:selavify/services/db_service.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await DBService.init();
-
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await DBService.init();
+  final db = SelavifyDB.withName("test_selavify.db");
   runApp(
-    MaterialApp(
+    MultiProvider(
+      providers: [
+        Provider(create: (_) => CategoryDao(db)),
+        Provider(create: (_) => SourceDao(db)),
+        Provider(create: (_) => TransactionTypeDao(db)),
+        Provider(create: (_) => TransactionDao(db)),
+      ],
+      child: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.red,
         inputDecorationTheme: InputDecorationTheme(
@@ -17,12 +39,17 @@ void main() async {
             focusedBorder: OutlineInputBorder()),
       ),
       debugShowCheckedModeBanner: false,
-      home: TransactionList(),
+      home: CategoryList(),
       initialRoute: '/',
       routes: {
-        TransactionList.routeName: (context) => TransactionList(),
-        AddTransaction.routeName: (context) => AddTransaction(),
+        CategoryList.routeName: (context) => CategoryList(),
+        SourceList.routeName: (context) => SourceList(),
+        TransactionTypeList.routeName: (context) => TransactionTypeList(),
+        TransactionsList.routeName: (context) => TransactionsList()
+        // SourceList.routeName: (context) => SourceList(),
+        // TransactionList.routeName: (context) => TransactionList(),
+        // AddTransaction.routeName: (context) => AddTransaction(),
       },
-    ),
-  );
+    );
+  }
 }
