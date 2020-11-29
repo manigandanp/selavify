5585ba0a7382839dc8ff40b918dda02ebe657c46
Manigandan P <manigandan.p@avalara.com>
2020-11-29 09:48:01 +0530

    update add_transaction page

--- lib/pages/add_transaction.dart
+++ lib/pages/add_transaction.dart
@@ -1,187 +1,203 @@
-// import 'dart:async';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_form_builder/flutter_form_builder.dart';
-// import 'package:intl/intl.dart';
-// import 'package:selavify/models/category.dart';
-// import 'package:selavify/models/source.dart';
-// import 'package:selavify/models/transaction.dart';
-// import 'package:selavify/models/transaction_type.dart';
-// import 'package:selavify/services/db_service.dart';
-// import 'package:selavify/services/transaction_service.dart';
-// import 'package:selavify/widgets/custom_dropdown.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:selavify/models/app_database.dart';
+import 'package:selavify/models/dao.dart';
 
-// class AddTransaction extends StatefulWidget {
-//   static final routeName = "/add_transaction";
+class AddNewTransaction extends StatelessWidget {
+  final String title;
+  final NewTransaction oldTransaction;
 
-//   @override
-//   _AddTransactionState createState() => _AddTransactionState();
-// }
+  AddNewTransaction({
+    this.title,
+    this.oldTransaction,
+  });
 
-// class _AddTransactionState extends State<AddTransaction> {
-//   final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+  static final routeName = "/addNewTranasaction";
+  final GlobalKey _fbKey = GlobalKey<FormBuilderState>();
 
-//   final _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    final categoryDao = Provider.of<CategoryDao>(context);
+    final sourceDao = Provider.of<SourceDao>(context);
+    final tTypeDao = Provider.of<TransactionTypeDao>(context);
+    final transactionDao = Provider.of<TransactionDao>(context);
+    final isEditPage = oldTransaction != null;
+    return Scaffold(
+      appBar: AppBar(title: Text(title ?? "Add New Transaction")),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: FormBuilder(
+            key: _fbKey,
+            child: Column(
+              children: [
+                FormBuilderDateTimePicker(
+                  fieldLabelText: "Choose Date",
+                  name: "transactionTimestamp",
+                  inputType: InputType.both,
+                  initialValue: isEditPage
+                      ? DateTime.fromMillisecondsSinceEpoch(
+                          oldTransaction.transactionTimestamp)
+                      : DateTime.now(),
+                  format: DateFormat("dd-MM-yyyy hh:ss"),
+                  resetIcon: null,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(context),
+                  ]),
+                  valueTransformer: (dateStr) => dateStr.millisecondsSinceEpoch,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                FormBuilderTextField(
+                  name: "transactionTitle",
+                  decoration: InputDecoration(
+                    labelText: "Transaction Title",
+                    hintText: "Transaction Title",
+                  ),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(context),
+                  ]),
+                  initialValue: isEditPage ? oldTransaction.title : "",
+                ),
+                SizedBox(height: 10),
+                FormBuilderTextField(
+                    name: "amount",
+                    decoration: InputDecoration(
+                      labelText: "Transaction Amount",
+                      hintText: "Transaction Amount",
+                    ),
+                    keyboardType: TextInputType.number,
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(context),
+                    ]),
+                    valueTransformer: (val) => double.parse(val),
+                    initialValue:
+                        isEditPage ? oldTransaction.amount.toString() : ""),
+                SizedBox(
+                  height: 10,
+                ),
+                StreamBuilder<List<Category>>(
+                  stream: categoryDao.watchCategories(),
+                  builder: (context, AsyncSnapshot<List<Category>> snapshot) {
+                    final items = snapshot.data ?? List();
+                    final initialValue =
+                        isEditPage ? oldTransaction.categoryId : null;
+                    return _dropdownBuilder(
+                        context, items, "Category", initialValue);
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                StreamBuilder<List<Source>>(
+                  stream: sourceDao.watchSources(),
+                  builder: (context, AsyncSnapshot<List<Source>> snapshot) {
+                    final items = snapshot.data ?? List();
+                    final initialValue =
+                        isEditPage ? oldTransaction.sourceId : null;
+                    return _dropdownBuilder(
+                        context, items, "Source", initialValue);
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                StreamBuilder<List<TransactionType>>(
+                  stream: tTypeDao.watchTransactionTypes(),
+                  builder:
+                      (context, AsyncSnapshot<List<TransactionType>> snapshot) {
+                    final items = snapshot.data ?? List();
+                    final initialValue =
+                        isEditPage ? oldTransaction.transactionTypeId : null;
+                    return _dropdownBuilder(
+                        context, items, "Transaction Type", initialValue);
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  color: Theme.of(context).primaryColor,
+                  child: ElevatedButton(
+                    onPressed: () =>
+                        _submitHandler(context, transactionDao, isEditPage),
+                    child: Text(
+                      "Save",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-//   Future<Map<String, dynamic>> dropdownValues;
+  void _submitHandler(
+      BuildContext context, TransactionDao transactionDao, bool isEditPage) {
+    GlobalKey<FormBuilderState> fbKey = _fbKey;
+    if (fbKey.currentState.saveAndValidate()) {
+      Map<String, dynamic> value = fbKey.currentState.value;
+      if (isEditPage) {
+        transactionDao.updateTransaction(
+          oldTransaction.copyWith(
+              title: value['transactionTitle'],
+              amount: value['amount'],
+              categoryId: value["category"],
+              sourceId: value["source"],
+              transactionTypeId: value["transaction_type"],
+              transactionTimestamp: value["transactionTimestamp"]),
+        );
+      } else {
+        transactionDao.createTransaction(
+          value['transactionTitle'],
+          value['amount'],
+          value["category"],
+          value["source"],
+          value["transaction_type"],
+          transactionTimestamp: value["transactionTimestamp"],
+        );
+      }
+      print(fbKey.currentState.value);
+      Navigator.pop(context);
+    } else
+      print("something went wrong");
+  }
 
-//   Widget forms({
-//     context,
-//     transactionTypeItems,
-//     sourceItems,
-//     categoryItems,
-//     formKey,
-//   }) =>
-//       FormBuilder(
-//         key: formKey,
-//         child: Column(
-//           children: [
-//             datePicker,
-//             spacer,
-//             transactionTypeCustomDropdown(transactionTypeItems),
-//             spacer,
-//             sourceCustomDropdown(sourceItems),
-//             spacer,
-//             categoryCustomDropdown(categoryItems),
-//             spacer,
-//             formTextField("transactionTitle", "Transaction Tilte"),
-//             spacer,
-//             transactionAmount,
-//             spacer,
-//             submitButton(context, formKey),
-//           ],
-//         ),
-//       );
+  FormBuilderDropdown _dropdownBuilder(BuildContext context, List items,
+          String labelText, String initialValue) =>
+      FormBuilderDropdown(
+        decoration: InputDecoration(
+          labelText: labelText,
+        ),
+        name: labelText.toLowerCase().split(" ").join("_"),
+        items: _menuItemBuilder(items),
+        validator: FormBuilderValidators.compose([
+          FormBuilderValidators.required(context),
+        ]),
+        initialValue: initialValue,
+      );
 
-//   final Widget spacer = SizedBox(
-//     height: 15,
-//   );
-
-//   Widget transactionTypeCustomDropdown(tType) => DropdownWithBottomModal(
-//         attribute: "transactionTypeId",
-//         items: tType,
-//         hint: "Transaction Type",
-//         childForm: formTextField("transactionType", "Transaction Type"),
-//       );
-
-//   Widget sourceCustomDropdown(sources) => DropdownWithBottomModal(
-//         attribute: "sourceId",
-//         items: sources,
-//         hint: "Source",
-//         childForm: formTextField("source", "Source"),
-//       );
-
-//   Widget categoryCustomDropdown(categories) => DropdownWithBottomModal(
-//         attribute: "categoryId",
-//         items: categories,
-//         hint: "Category",
-//         childForm: formTextField("category", "Category"),
-//       );
-
-//   final Widget datePicker = FormBuilderDateTimePicker(
-//     attribute: 'transactionTimestamp',
-//     inputType: InputType.both,
-//     initialValue: DateTime.now(),
-//     validators: [
-//       FormBuilderValidators.required(),
-//     ],
-//     format: DateFormat("dd-MM-yyyy hh:ss"),
-//     resetIcon: null,
-//     valueTransformer: (dateStr) => dateStr.millisecondsSinceEpoch,
-//   );
-
-//   Widget formTextField(attribute, labelText) => FormBuilderTextField(
-//         attribute: attribute, //"transactionTitle",
-//         decoration: InputDecoration(
-//           labelText: labelText, //"Transaction Title",
-//           hintText: labelText, //"Title",
-//         ),
-//         validators: [
-//           FormBuilderValidators.required(),
-//         ],
-//       );
-
-//   final Widget transactionAmount = FormBuilderTextField(
-//     attribute: "transactionAmount",
-//     decoration: InputDecoration(
-//       labelText: "Transaction Amount",
-//       hintText: "Amount",
-//     ),
-//     keyboardType: TextInputType.number,
-//     validators: [
-//       FormBuilderValidators.required(),
-//       FormBuilderValidators.numeric()
-//     ],
-//   );
-
-//   void _submitAction(formKey) async {
-//     if (formKey.currentState.saveAndValidate()) {
-//       Map<String, dynamic> response =
-//           await TransactionService.addTransaction(formKey.currentState.value);
-//       Navigator.of(context).pop(Transaction.fromJson(response));
-//     } else {
-//       _scaffoldKey.currentState.hideCurrentSnackBar();
-//       _scaffoldKey.currentState.showSnackBar(
-//         SnackBar(
-//           content: Text("Something went wrong"),
-//         ),
-//       );
-//     }
-//   }
-
-//   Widget submitButton(context, formKey) => ElevatedButton(
-//         onPressed: () => _submitAction(formKey),
-//         child: Text(
-//           "Submit",
-//           style: TextStyle(
-//             fontSize: 16,
-//             fontWeight: FontWeight.bold,
-//           ),
-//         ),
-//       );
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     dropdownValues = DBService.queryDefaultValues();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       key: _scaffoldKey,
-//       appBar: AppBar(
-//         title: Text("Add Transaction"),
-//       ),
-//       body: FutureBuilder(
-//         future: dropdownValues,
-//         builder: (context, snapshot) {
-//           if (snapshot.hasError) {
-//             return Center(
-//               child: Text("Error loading dropdownValues " + snapshot.error),
-//             );
-//           } else if (snapshot.hasData) {
-//             List<TransactionType> tType = snapshot.data["transactionType"];
-//             List<Category> category = snapshot.data["category"];
-//             List<Source> source = snapshot.data["source"];
-
-//             return SingleChildScrollView(
-//               child: Padding(
-//                 child: forms(
-//                   context: context,
-//                   formKey: _fbKey,
-//                   transactionTypeItems: tType,
-//                   categoryItems: category,
-//                   sourceItems: source,
-//                 ),
-//                 padding: EdgeInsets.all(15),
-//               ),
-//             );
-//           } else
-//             return Center(
-//               child: CircularProgressIndicator(),
-//             );
-//         },
-//       ),
-//     );
-//   }
-// }
+  List<DropdownMenuItem> _menuItemBuilder(List items) {
+    List<DropdownMenuItem> dropDownItems = items
+        .map(
+          (item) => DropdownMenuItem(
+            value: item.id,
+            child: Text(item.title),
+          ),
+        )
+        .toList();
+    return [...dropDownItems];
+  }
+}
