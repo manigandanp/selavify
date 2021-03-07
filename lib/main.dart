f9aac3896f4da251274db25357734ce68f3c31ff
Manigandan P <manigandan.p@avalara.com>
2021-03-07 11:55:09 +0530

    delete old codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:selavify/models/app_database.dart';
-import 'package:selavify/models/dao.dart';
-import 'package:selavify/pages/dashboard_page.dart';
-import 'package:selavify/routes.dart';
-
 void main() async {
-  final db = SelavifyDB.withName("test_selavify.db");
-  runApp(
-    MultiProvider(
-      providers: [
-        Provider(create: (_) => CategoryDao(db)),
-        Provider(create: (_) => SourceDao(db)),
-        Provider(create: (_) => TransactionTypeDao(db)),
-        Provider(create: (_) => TransactionDao(db)),
-      ],
-      child: HomePage(),
-    ),
-  );
+
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(),
-          enabledBorder: OutlineInputBorder(),
-          focusedBorder: OutlineInputBorder(),
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Dashboard(),
-      initialRoute: '/',
-      routes: routes,
-    );
-  }
-}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,15 +1,15 @@
-import 'package:selavify/pages/add_transaction.dart';
-import 'package:selavify/pages/categories.dart';
-import 'package:selavify/pages/dashboard_page.dart';
-import 'package:selavify/pages/sources.dart';
-import 'package:selavify/pages/transaction_type.dart';
-import 'package:selavify/pages/transactions.dart';
+// import 'package:selavify/pages/add_transaction.dart';
+// import 'package:selavify/pages/categories.dart';
+// import 'package:selavify/pages/dashboard_page.dart';
+// import 'package:selavify/pages/sources.dart';
+// import 'package:selavify/pages/transaction_type.dart';
+// import 'package:selavify/pages/transactions.dart';
 
-final routes = {
-  CategoryList.routeName: (context) => CategoryList(),
-  SourceList.routeName: (context) => SourceList(),
-  TransactionTypeList.routeName: (context) => TransactionTypeList(),
-  TransactionsList.routeName: (context) => TransactionsList(),
-  AddNewTransaction.routeName: (context) => AddNewTransaction(),
-  Dashboard.routeName: (context) => Dashboard(),
-};
+// final routes = {
+//   CategoryList.routeName: (context) => CategoryList(),
+//   SourceList.routeName: (context) => SourceList(),
+//   TransactionTypeList.routeName: (context) => TransactionTypeList(),
+//   TransactionsList.routeName: (context) => TransactionsList(),
+//   AddNewTransaction.routeName: (context) => AddNewTransaction(),
+//   Dashboard.routeName: (context) => Dashboard(),
+// };
