069e52ae2c362df0e42305afbeb910c2ed7588db
Manigandan P <manigandan.p@avalara.com>
2021-03-07 12:38:57 +0530

    add temp home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,22 @@
-void main() {
+import 'package:flutter/material.dart';
 
+void main() => runApp(HomePage());
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "HomePage",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Page"),
+        ),
+        body: Container(
+          child: Center(
+            child: Text("Home Page"),
+          ),
+        ),
+      ),
+    );
+  }
 }
-
