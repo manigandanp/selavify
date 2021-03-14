fc0b6778acd2d1c60a6b4dd352db37776176c343
Manigandan P <manigandan.p@avalara.com>
2021-03-14 08:47:56 +0530

    implement remote auth

--- lib/auth/data/remote_auth_data.dart
+++ lib/auth/data/remote_auth_data.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:selavify/auth/models/user_model.dart';
-import 'package:selavify/common/network_info.dart';
 
 abstract class RemoteAuthData {
   Future<UserModel> signIn();
@@ -7,19 +8,22 @@ abstract class RemoteAuthData {
 }
 
 class GoogleAuth implements RemoteAuthData {
-  final NetworkInfo _networkInfo;
-  
-  GoogleAuth(this._networkInfo);
+  final GoogleSignIn googleSignIn;
+
+  GoogleAuth({@required this.googleSignIn});
 
   @override
-  Future<UserModel> signIn() {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  Future<UserModel> signIn() async {
+    GoogleSignInAccount account = await googleSignIn.signInSilently();
+    account ??= await googleSignIn.signIn();
+    return UserModel(
+        userName: account.displayName,
+        email: account.email,
+        avatarUrl: account.photoUrl);
   }
 
   @override
-  Future<void> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
+  Future<void> signOut() async {
+    return await googleSignIn.signOut();
   }
 }
